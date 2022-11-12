36e010b38cee612b4e817f8e9ef605e58b59f545
adoyi <adoyiowoicho@gmail.com>
2022-11-12 17:28:32 +0100

    overhidden and features edit

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 ///////////////////////////////////////////////////////////
 //Make mobile Navigation work
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
 });
 
 ///////////////////////////////////////////////////////////
 //making links work
-const allLinks = document.querySelectorAll("a:link");
+const allLinks = document.querySelectorAll('a:link');
 // console.log(allLinks);
 
 allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
+  link.addEventListener('click', function (e) {
     e.preventDefault();
-    const href = link.getAttribute("href");
+    const href = link.getAttribute('href');
     console.log(href);
 
     ///link "#" to lead back to home page
-    if (href === "#")
+    if (href === '#')
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
 
     // scroll into other links
-    if (href !== "#" && href.startsWith("#")) {
+    if (href !== '#' && href.startsWith('#')) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
     }
 
-    if (link.classList.contains("main-nav-link")) {
-      headerEl.classList.toggle("nav-open");
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
     }
   });
 });
@@ -40,7 +40,7 @@ allLinks.forEach(function (link) {
 ///////////////////////////////////////////////////////////
 //Sticky Navigation
 
-const sectionHeroEl = document.querySelector(".section-hero");
+const sectionHeroEl = document.querySelector('.section-hero');
 
 const obs = new IntersectionObserver(
   function (entries) {
@@ -48,18 +48,18 @@ const obs = new IntersectionObserver(
 
     if (ent.isIntersecting === false) {
       console.log(ent);
-      document.body.classList.add("sticky");
+      document.body.classList.add('sticky');
     }
 
     if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
+      document.body.classList.remove('sticky');
     }
   },
   {
     //observe in the viewport
     root: null,
     threshold: 0,
-    rootMargin: "-80px",
+    rootMargin: '-80px',
   }
 );
 obs.observe(sectionHeroEl);
@@ -67,34 +67,50 @@ obs.observe(sectionHeroEl);
 ////////////////////////////////////////////
 //Making the feature link work
 
-const bookMarkingEl = document.querySelector(".bookmarking-box");
-const searchingEl = document.querySelector(".searching-box");
-const sharingEl = document.querySelector(".sharing-box");
+const bookMarkingEl = document.querySelector('.bookmarking-box');
+const searchingEl = document.querySelector('.searching-box');
+const sharingEl = document.querySelector('.sharing-box');
 
-bookMarkingEl.addEventListener("click", function () {
-  document.querySelector(".bookmarking").style.display = "block";
-  document.querySelector(".searching").style.display = "none";
-  document.querySelector(".sharing").style.display = "none";
+bookMarkingEl.addEventListener('click', function () {
+  document.querySelector('.bookmarking').style.display = 'block';
+  document.querySelector('.searching').style.display = 'none';
+  document.querySelector('.sharing').style.display = 'none';
 });
 
-searchingEl.addEventListener("click", function () {
-  document.querySelector(".bookmarking").style.display = "none";
-  document.querySelector(".searching").style.display = "block";
-  document.querySelector(".sharing").style.display = "none";
+searchingEl.addEventListener('click', function () {
+  document.querySelector('.bookmarking').style.display = 'none';
+  document.querySelector('.searching').style.display = 'block';
+  document.querySelector('.sharing').style.display = 'none';
 });
 
-sharingEl.addEventListener("click", function () {
-  document.querySelector(".bookmarking").style.display = "none";
-  document.querySelector(".searching").style.display = "none";
-  document.querySelector(".sharing").style.display = "block";
+sharingEl.addEventListener('click', function () {
+  document.querySelector('.bookmarking').style.display = 'none';
+  document.querySelector('.searching').style.display = 'none';
+  document.querySelector('.sharing').style.display = 'block';
 });
+
+// const featureEl = document.querySelectorAll('.feature');
+// const featurePlanEl = document.querySelectorAll('.feature-plan');
+
+// // for (let i = o; i < featureEl.length; i++)
+// //   featureEl[i].addEventListener('click', function () {
+// //     for (let i = 0; i < featurePlanEl.length; i++)
+// //       featurePlanEl[i].classList.toggle('activet');
+// //     // featurePlanEl[i].classList.remove('hiddent');
+// //   });
+
+// featureEl.forEach(function (featurePlanEl) {
+//   featureEl.addEventListener('click', function () {
+//     featurePlanEl.classList.toggle('active');
+//   });
+// });
 
 //////////////////////////////////////////////////////////////////
 //Making FAQ work
 
-const faqs = document.querySelectorAll(".FQA-item ");
+const faqs = document.querySelectorAll('.FQA-item ');
 faqs.forEach(function (faq) {
-  faq.addEventListener("click", function () {
-    faq.classList.toggle("active");
+  faq.addEventListener('click', function () {
+    faq.classList.toggle('active');
   });
 });
